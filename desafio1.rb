257cc28efb187c789d26c6452ea7ae286fe70e18
Antonio Junior <ajr3090@gmail.com>
2025-04-21 18:07:16 -0300

    Adiciona script para ler nome e idade do usuário

---
+++ desafio1.rb
@@ -0,0 +1,9 @@
+puts "Por favor, digite seu nome e seobrenome: "
+
+nome_completo = gets.chomp
+
+puts "Agora digite a sua idade: "
+
+idade = gets.to_i
+
+puts "Seja bem vindo, #{nome_completo}, sabemos que tem #{idade}"
